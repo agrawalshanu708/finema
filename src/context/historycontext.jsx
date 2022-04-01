c6065f7a16ab430a2b788605700922f27fa523c0
shanu agrawal <agrawalshanu798@gmail.com>
2022-04-01 20:33:59 +0530

    create reducer template

--- src/context/historycontext.jsx
+++ src/context/historycontext.jsx
@@ -1,11 +1,13 @@
 import { useContext, createContext,useReducer } from "react";
-
+import {historyReducer} from "./../reducer/index"
 const HistoryContext = createContext();
 const HistoryProvider = ({children}) => {
-  
+  const[historyState,historyDispatch] = useReducer(historyReducer,{
+     historyItems: [] 
+  })
  
     return (
-        <HistoryContext.Provider value = {{}}>
+        <HistoryContext.Provider value = {{historyState,historyDispatch}}>
             {children}
         </HistoryContext.Provider>
     )
---
+++ src/reducer/historyreducer.jsx
@@ -0,0 +1,11 @@
+const historyReducer = (historyState,historyAction) => {
+    switch (historyAction.type) {
+        case "":
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+export {historyReducer}
