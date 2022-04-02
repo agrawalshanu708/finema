c0d9819dd6d3986b8e00f81e9b03df84aded46a3
shanu agrawal <agrawalshanu798@gmail.com>
2022-04-03 00:29:43 +0530

    feat: add some feature to watchlater

--- src/pages/watchlaterpage/watchlatercard.jsx
+++ src/pages/watchlaterpage/watchlatercard.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import {AiOutlineHeart} from "react-icons/ai"
 import {MdOutlinePlaylistPlay} from "react-icons/md"
 import {MdOutlineWatchLater} from"react-icons/md"
-import {useWatchLater} from "./../../context/index"
+import {useWatchLater,useLike} from "./../../context/index"
+import { checkInWatch } from '../../utils/index'
+
 const WatchLaterCard = ({product,index}) => {
     const{_id,title,description,charactor} = product
     const{watchLaterDispatch} = useWatchLater()
+    const{likeState,likeDispatch} = useLike()
 
 
- 
+    const isItem = checkInWatch(_id,likeState.likeItems)
+    const likeHandler = (id,product) => {
+      if(isItem){
+   likeDispatch({
+        type:"REMOVE_FROM_LIKE",
+        payload: id
+      })
+      }else{
+        likeDispatch({ 
+                type:"ADD_TO_LIKE",
+                payload : product
+              })
+      }
+    }
 
     return (
     <div class="border-skin text-overlay-card-dimension card-relative video-card" key = {index}>
@@ -23,10 +39,7 @@ const WatchLaterCard = ({product,index}) => {
       Visit ten places on our planet that are undergoing the biggest
       changes today</div>
     <div class="card-footer-box card__icons">
-      <AiOutlineHeart color = "#AB542F" size="3rem" onClick = {() => watchLaterDispatch({
-        type:"REMOVE_FROM_LIKE",
-        payload: index
-        })}/>
+      <AiOutlineHeart color = "#AB542F" size="3rem" onClick = {() => likeHandler(_id,product)}/>
       <MdOutlinePlaylistPlay color= "#ffff" size="4rem"/>
       <MdOutlineWatchLater color= "#ffff" size="3rem" onClick = {() => watchLaterDispatch({
         type: "REMOVE_FROM_WATCH_LATER",
