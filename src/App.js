145c32cad4fcdbaf197596cdad4fa870a8546db1
shanu agrawal <agrawalshanu798@gmail.com>
2022-06-10 15:26:49 +0530

    feat: add favicon & title

--- src/App.js
+++ src/App.js
@@ -1,63 +1,89 @@
 import "./App.css";
-import { Routes, Route, useLocation} from "react-router-dom";
-import {Navbar,Searchbar,RequireAuth,PlainNav} from "./components/index"
-import {Home,LikePage,WatchLaterPage,HistoryPage,Playlist,Login,Signup,LandingPage, Error404, SingleVideo} from "./pages/index"
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Navbar, Searchbar, RequireAuth, PlainNav } from "./components/index";
+import {
+  Home,
+  LikePage,
+  WatchLaterPage,
+  HistoryPage,
+  Playlist,
+  Login,
+  Signup,
+  LandingPage,
+  Error404,
+  SingleVideo,
+} from "./pages/index";
 import { VideosInPlaylist } from "./pages/playlistpage/videosInPlaylist";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 
 function App() {
   const { pathname } = useLocation();
+  useEffect(() => {
+    document.title = "Finema";
+  }, []);
 
   return (
     <div className="App">
-    {pathname === "/login" || pathname === "/signup" ? (
+      {pathname === "/login" || pathname === "/signup" ? (
         <PlainNav />
-      ): (
+      ) : (
         <Navbar />
       )}
-      <Searchbar/>
-    <Routes>
-      <Route path = "*" element = {<Error404/>}/>
-      <Route path="/videos" element = {<Home/>}/>
-      <Route path = "/" element = {<LandingPage/>}/>
-      <Route path = "/video/:videoId" element = {<SingleVideo/>}/>
-      <Route path = "/like" element = {
-      <RequireAuth>
-      <LikePage/>
-      </RequireAuth>
-      }/>
-      <Route path = "/watchlater" element = {
-      <RequireAuth>
-      <WatchLaterPage/>
-      </RequireAuth>
-      }/>
-      <Route path = "/history" element = {
-      <RequireAuth>
-      <HistoryPage/>
-      </RequireAuth>
-      }/>
-      <Route path = "/playlist" element = {
-      <RequireAuth>
-      <Playlist/>
-      </RequireAuth>
-      }/>
-      <Route path = "/login" element = {<Login/>}/>
-      <Route path = "/signup" element = {<Signup/>}/>
-      <Route path="/playlist/:id" element={<VideosInPlaylist/>} />
-    </Routes>
-    <ToastContainer
-position="top-center"
-autoClose={2000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-
-/>
+      <Searchbar />
+      <Routes>
+        <Route path="*" element={<Error404 />} />
+        <Route path="/videos" element={<Home />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/video/:videoId" element={<SingleVideo />} />
+        <Route
+          path="/like"
+          element={
+            <RequireAuth>
+              <LikePage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/watchlater"
+          element={
+            <RequireAuth>
+              <WatchLaterPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <RequireAuth>
+              <HistoryPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/playlist"
+          element={
+            <RequireAuth>
+              <Playlist />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/playlist/:id" element={<VideosInPlaylist />} />
+      </Routes>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
